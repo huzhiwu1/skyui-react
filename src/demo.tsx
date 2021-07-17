b20c034f01600be6575f084e3a1e51bc75ef02cc
huzhiwu1 <1627983849@qq.com>
2021-07-18 01:06:35 +0800

    feat: webpack支持打包输出umd

---
+++ src/demo.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const Demo: FC = function () {
+	return <div>我是demo</div>;
+};
+
+export default React.memo(Demo);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const Path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+module.exports = {
+	mode: 'development',
+	entry: {
+		index: Path.resolve(__dirname, './src/index.ts'),
+	},
+	output: {
+		path: Path.resolve(__dirname, 'dist'),
+		libraryTarget: 'umd',
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.jsx'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+			},
+		],
+	},
+	externals: {
+		react: {
+			commonjs: 'react',
+			commonjs2: 'react',
+			amd: 'react',
+			root: 'React',
+		},
+		'react-dom': {
+			commonjs: 'react-dom',
+			commonjs2: 'react',
+			amd: 'react-dom',
+			root: 'ReactDOM',
+		},
+	},
+	plugins: [new CleanWebpackPlugin()],
+};
