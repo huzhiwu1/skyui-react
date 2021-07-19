015e0a91c612a2a88bd75081e8bde96b9cbcb928
huzhiwu1 <1627983849@qq.com>
2021-07-20 00:10:17 +0800

    feat: webpack添加eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,37 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true,
-	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:@typescript-eslint/recommended',
-	],
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
-		'ecmaFeatures': {
-			'jsx': true,
-		},
-		'ecmaVersion': 12,
-		'sourceType': 'module',
-	},
-	'plugins': ['react', '@typescript-eslint'],
-	'rules': {
-		'indent': ['error', 'tab'],
-		'linebreak-style': ['error', 'unix'],
-		'quotes': ['error', 'single'],
-		'semi': ['error', 'always'],
-	},
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    // 提示要在jsx文件里手动引入React
+    'react/react-in-jsx-scope': [2],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      extends: ['airbnb-typescript'],
+      rules: {
+        'func-names': 0,
+        'import/export': 0,
+        '@typescript-eslint/indent': ['error', 2],
+        '@typescript-eslint/object-curly-spacing': [0], // 0无所谓，1警告，2报错
+        'import/prefer-default-export': [0],
+      },
+    },
+  ],
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,45 @@
 const Path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslintWebpackPlugin = require('eslint-webpack-plugin');
+
 module.exports = {
-	mode: 'development',
-	entry: {
-		index: Path.resolve(__dirname, './src/index.ts'),
-	},
-	output: {
-		path: Path.resolve(__dirname, 'dist'),
-		libraryTarget: 'umd',
-	},
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js', '.jsx'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-			},
-		],
-	},
-	externals: {
-		react: {
-			commonjs: 'react',
-			commonjs2: 'react',
-			amd: 'react',
-			root: 'React',
-		},
-		'react-dom': {
-			commonjs: 'react-dom',
-			commonjs2: 'react',
-			amd: 'react-dom',
-			root: 'ReactDOM',
-		},
-	},
-	plugins: [new CleanWebpackPlugin()],
+  mode: 'development',
+  entry: {
+    index: Path.resolve(__dirname, './src/index.ts'),
+  },
+  output: {
+    path: Path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    },
+  },
+  plugins: [
+    new EslintWebpackPlugin({
+      extensions: ['.ts', '.tsx'],
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
