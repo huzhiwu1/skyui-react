283ca506a4072287aafc01183b27d1ff7430af9f
huzhiwu1 <1627983849@qq.com>
2021-07-29 00:12:30 +0800

    test(icon): 使用enzyme测试

--- src/components/Icon/__tests__/index.test.tsx
+++ src/components/Icon/__tests__/index.test.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+// import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { Icon } from '../index';
 
+// Enzyme.configure({ adapter: new Adapter() });
 describe('测试Icon', () => {
   it('测试icon能成功渲染', () => {
     const json = TestRenderer.create(<Icon name="sync" />).toJSON();
     expect(json).toMatchSnapshot();
+  });
+
+  it('测试 onClick', () => {
+    const fn = jest.fn();
+    const wrapper = mount(<Icon name="sync" onClick={fn} />);
+    wrapper.find('.sky-icon').simulate('click');
+    expect(fn.mock.calls.length).toEqual(1);
   });
 });
