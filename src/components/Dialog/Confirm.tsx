7983660716ed4f2e6ca7b44bfa549d1330fb5fd2
huzhiwu1 <1627983849@qq.com>
2021-08-06 23:59:09 +0800

    feat(dialog): 添加更新dialog的函数

--- src/components/Dialog/Confirm.tsx
+++ src/components/Dialog/Confirm.tsx
@@ -2,20 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Dialog, { DialogProps } from './Dialog';
 
-export default function (config: DialogProps) {
+export type ConfirmProps = Omit<DialogProps, 'children'> & {
+  content: React.ReactNode;
+};
+
+export type UpdateConfirm =
+  | ConfirmProps
+  | ((preConfig: ConfirmProps) => ConfirmProps);
+
+export default function (config: ConfirmProps) {
   const div = document.createElement('div');
   document.body.appendChild(div);
 
-  function render() {
-    ReactDOM.render(<Dialog {...config} />, div);
-  }
+  let currentConfig: ConfirmProps = {
+    ...config,
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    onCancel: close,
+    visible: true,
+  };
 
-  render();
+  function render({ content, ...props }: ConfirmProps) {
+    ReactDOM.render(<Dialog {...props}>{content}</Dialog>, div);
+  }
 
   function destroy() {
     ReactDOM.unmountComponentAtNode(div);
   }
+
+  function update(updater: UpdateConfirm) {
+    if (typeof updater === 'function') {
+      currentConfig = updater(currentConfig);
+    }
+    currentConfig = {
+      ...currentConfig,
+      ...updater,
+    };
+    render(currentConfig);
+  }
+
+  function close() {
+    currentConfig = {
+      ...currentConfig,
+      visible: false,
+    };
+    render(currentConfig);
+  }
+
+  render(currentConfig);
+
   return {
     destroy,
+    update,
   };
 }
--- src/components/Dialog/index.ts
+++ src/components/Dialog/index.ts
@@ -1,10 +1,12 @@
-import Dialog, { DialogProps, DialogType } from './Dialog';
-import Confirm from './Confirm';
+import Dialog, { DialogType } from './Dialog';
+import Confirm, { ConfirmProps, UpdateConfirm } from './Confirm';
 
 export * from './Dialog';
 
 interface ModalType extends DialogType {
-  confirm(config: DialogProps): { destroy(): void };
+  confirm(
+    config: ConfirmProps,
+  ): { destroy(): void; update(config: UpdateConfirm): void };
 }
 const Modal = Dialog as ModalType;
 Modal.confirm = Confirm;
